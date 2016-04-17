f6e4e455f63e2b1b88d58d7fb0a93ec1b1e0395e
frank boakye yiadom <fbyiadom@gmail.com>
2016-04-17 12:05:53 -0400

    add docs views

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,40 +1,45 @@
 class DocsController < ApplicationController
+	before_action :find_doc,only: [:show, :edit, :update, :destroy]
 
 	def index
-		
+	@docs = Doc.all.order('created_at DESC')	
 	end
 
 	def show
-		
 	end
 
 	def new
+		@doc = Doc.new
 		
 	end
 
 	def create
+		@doc = Doc.new(doc_params)
+
+		if @doc.save
+			redirect_to @doc
+		else
+			render 'new'
+		end
 		
 	end
 
-	def edit
-		
+	def edit	
 	end
 
-	def update
-		
+	def update		
 	end
 
-	def destroy
-		
+	def destroy	
 	end
 
-	private
-
+private
 	def find_doc
-		
+	@doc = Doc.find(params[:id])	
 	end
 
 	def doc_params
+		params.require(:doc).permit(:title, :content)
 		
 	end
 end
